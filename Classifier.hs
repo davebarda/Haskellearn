0552e4dc94f5c63283affbec50dc997bb52ccd18
David Barda <davebarda@gmail.com>
2016-09-11 13:55:27 +0300

    Done with simple knn and added types of it (regression and classification)

--- Classifier.hs
+++ Classifier.hs
@@ -1,14 +1,46 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
+import Data.List
 
-module Classifier
-(
-    Classifier,
-    train,
-    classify,
-    Classifier.error
-) where
+--
+data Classifier = KNN {
+                k :: Int,
+                norm :: Norm
+               }  | SVM
 
-class Classifier classifier knowledge x y where
-  train :: classifier -> [x] -> [y] -> knowledge
-  classify :: classifier -> knowledge -> x -> y
-  error :: classifier -> knowledge -> [x] -> [y] -> Float
+--
+type Norm = [Double] -> Double
+
+--
+data TrainingKnowledge = KNNKnowledge [[Double]] [Double] Int | SVMKnowledge deriving Show
+
+--
+data Classification = KNNRegression Double | KNNClassification Int | SVMClassification deriving Show
+
+--
+train :: Classifier -> [[Double]] -> [Double] -> TrainingKnowledge
+train (KNN k norm) xs ys = KNNKnowledge xs ys (length ys)
+
+--
+classify :: Classifier -> TrainingKnowledge -> [Double] -> Classification
+classify classifier@(KNN k norm) knowledge@(KNNKnowledge x y len) toClassify = sum (orderByDistance classifier knowledge toClassify) / fromIntegral len
+
+--
+orderByDistance :: Classifier -> TrainingKnowledge -> [Double] -> [Double]
+orderByDistance (KNN k norm) (KNNKnowledge x y _) toClassify = snd $ unzip $ take k $ sort $ zip (distanceFromVector norm x toClassify) y
+
+--
+distanceFromVector :: Norm ->  [[Double]] -> [Double] -> [Double]
+distanceFromVector norm x toClassify = map (norm . zipWith (-) toClassify) x
+
+--
+error :: Classifier -> TrainingKnowledge -> [[Double]] -> [Double] -> Double
+error (KNN k norm) knowledge xs ys = sum [(binaryLoss curY_hat curY) | (curY_hat, curY) <- zip y_hat ys] / (fromIntegral (length ys))
+  where
+    y_hat = map (classify (KNN k norm) knowledge) xs
+
+--
+lNorm :: Double -> [Double] -> Double
+lNorm n x = sum (map (** n) x) ** (1 / n)
+
+--
+binaryLoss :: (Eq a) => a -> a -> Double
+binaryLoss x y = if x /= y then 1.0 else 0.0
