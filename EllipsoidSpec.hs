ad40f0067acc4484a35e1bf54dca3d41f9806a6a
David Barda <davebarda@gmail.com>
2016-09-19 12:59:02 +0300

    Added test for Ellipsoid batch learning

--- EllipsoidSpec.hs
+++ EllipsoidSpec.hs
@@ -7,20 +7,26 @@ import Data.Matrix
 
 main :: IO ()
 main = hspec $ do
-  let know = initKnowledge (EllipsoidParameters 2)
-
+  let knowledge = initKnowledge (EllipsoidParameters 2)
   let eta = 4 / 3
+
+  let initialKnowledge = EllipsoidKnowledge 2 eta (identity 2) (zero 1 2)
+  let train1Knowledge = EllipsoidKnowledge 2 eta (fromList 2 2 [4 / 9, 0, 0, 4 / 3]) (fromList 1 2 [1 / 3, 0])
+
   let x1 = fromList 2 1 [1, 0]
   let y1 = LInt 1
 
   describe "Init tests" $
     it "Initialization test" $
-      know `shouldBe` EllipsoidKnowledge 2 eta (identity 2) (zero 1 2)
+      knowledge `shouldBe` initialKnowledge
 
   describe "Train tests" $ do
-    let knowTrain = train know x1 y1
-    let correctW = fromList 1 2 [1 / 3, 0]
-    let correctA  = fromList 2 2 [4 / 9, 0, 0, 4 / 3]
+    let knowTrain = train knowledge x1 y1
 
     it "Train on one example" $
-      knowTrain `shouldBe` EllipsoidKnowledge 2 eta correctA correctW
+      knowTrain `shouldBe` train1Knowledge
+
+  describe "Batch tests" $
+    it "Train on one example" $ do
+      let batchKnowledge = batch knowledge [x1] [y1]
+      batchKnowledge `shouldBe` train1Knowledge
--- OnlineLearner.hs
+++ OnlineLearner.hs
@@ -1,5 +1,5 @@
 module OnlineLearner(TrainingKnowledge(..), LearnerParameters(..),
-    initKnowledge, train, classify) where
+    initKnowledge, train, classify, batch) where
 
 import Label
 import Data.Matrix
@@ -32,16 +32,22 @@ train knowledge@(EllipsoidKnowledge d eta a w) example (LInt trueY) =
     ax = a * example
     xax = (transpose example * ax) ! (1, 1)
     dDouble = fromInteger $ toInteger d
-    newW = w + scaleMatrix (fromIntegral trueY / ((dDouble + 1) * sqrt xax)) (transpose ax :: Matrix Double)
+    newW = w + scaleMatrix (fromIntegral trueY / ((dDouble + 1) * sqrt xax)) (transpose ax)
     newA = scaleMatrix eta (a - scaleMatrix (2 / ((dDouble + 1) * xax))  ax * transpose ax)
     newKnowledge = EllipsoidKnowledge d eta newA newW
 
 train _ _ _ = Prelude.error "Train doesn't support these types"
 
 --
---batch :: TrainingKnowledge -> [Example] -> [Label] -> TrainingKnowledge
-
-
---
 classify :: TrainingKnowledge -> Example -> Label
 classify (EllipsoidKnowledge _ _ _ w) example = LInt $ round (signum (w  * example) ! (1, 1))
+
+--
+batch :: TrainingKnowledge -> [Example] -> [Label] -> TrainingKnowledge
+batch knowledge [] _ = knowledge
+batch knowledge _ [] = knowledge
+batch knowledge examples@(x:xs) labels@(y:ys) =
+  if correctLength then batch (train knowledge x y) xs ys else errorReturn
+  where
+    correctLength = length examples == length labels
+    errorReturn = Prelude.error "Examples length mismatch labels length"
